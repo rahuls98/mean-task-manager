ef3f3d46d51068dd775b1a24b2a3ec06620964c8
rahuls98 <rahs98@gmail.com>
2020-05-15 17:18:12 +0530

    new routes

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ var db;
 var app = express();
 app.use(bodyParser.json());
 
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 mongodb.MongoClient.connect(uri, function (err, client) {
-    if (err) {
+    if(err) {
         console.log(err);
         process.exit(1);
     }
@@ -22,7 +25,7 @@ mongodb.MongoClient.connect(uri, function (err, client) {
     console.log("\nDatabase connection ready");
 
     // Initialize the app.
-    var server = app.listen(process.env.PORT || 8080, function () {
+    var server = app.listen(process.env.PORT || 8000, function() {
         var port = server.address().port;
         console.log("App now running on port", port);
         console.log();
@@ -32,18 +35,31 @@ mongodb.MongoClient.connect(uri, function (err, client) {
 //API ROUTES
 app.get("/api/tasks", function(req, res) {
     db.collection(COLLECTION).find({}).toArray(function(err, tasks) {
-        if (err) res.send(err);
+        if(err) res.send(err);
 
-        console.log("Retrieved tasks!")
+        console.log("Retrieved tasks!");
         res.status(200).json(tasks);
     });
 });
 
 app.get("/api/task/:id", function(req, res) {
     db.collection(COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, task) {
-        if (err) res.send(err);
+        if(err) res.send(err);
 
-        console.log("Retrieved task!")
+        console.log("Retrieved task!");
         res.status(200).json(task);
     });
+});
+
+app.post("/api/task", function(req, res){
+    var newTask = req.body;
+
+    if (!req.body.title) res.send("Invalid user input");
+    
+    else {
+        db.collection(COLLECTION).insertOne(newTask, function(err, doc) {
+            if (err) res.send("Failed to insert!");
+            else res.status(201).json(doc.ops[0]);
+        });
+    }
 });
